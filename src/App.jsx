b6b95331052a2ddc26a20e295ea208b0b1f10403
Manisha3696 <manisharadha2004@gmail.com>
2025-07-22 16:12:52 +0530

    new changes
    new one

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Commingsoon from './Pages/Commingsoon';
+
+function App() {
+  return (
+    <div className="App">
+      <Commingsoon />
+    </div>
+  );
+}
+
+export default App;
